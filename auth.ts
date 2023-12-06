28a006980d1b08eb66f99af4c69c884c37cacd0e
cerealexperiments <prettysunshine468@gmail.com>
2023-12-07 00:54:59 +0600

    implement auth

--- auth.ts
+++ auth.ts
@@ -1,35 +1,50 @@
 import { FastifyInstance } from 'fastify';
+import { Pool } from 'pg';
 
-type UserType = "user" | "tailor";
-
-interface User {
-  username: string;
-  password: string; 
-  userType: UserType;
+type User = {
+  name: string;
+  email: string;
+  password: string;
+  profile_picture?: string;
+  additional_info?: string;
 }
 
-export default async function authRoutes(server: FastifyInstance) {
+type Tailor = {
+  name: string;
+  email: string;
+  password: string;
+  category?: string;
+  additionalInfo?: string;
+  profile_picture?: string;
+  portfolio?: string[];
+}
 
-  let users: User[] = [];
+export default async function authRoutes(server: FastifyInstance, pool: Pool ) {
 
-  server.post('/auth/register', async (request, reply) => {
-    const { username, password, userType = "user" } = request.body as User;
+  server.post('/auth/registerUser', async (request, reply) => {
+    const {name, email, password } = request.body as User;
+    await pool.query('INSERT INTO users (name, email, password) VALUES ($1, $2, $3)', [name, email, password]);
 
-    const alreadyExists = users.find(user => user.username === username);
-    if(alreadyExists) {
-      return { status: "error", message: `username ${username} already taken`}
+    return {
+      status: "success",
+      user: {
+        name, email, password
+      }
     }
-    users.push({ username, password, userType });
-    return { status: 'success',  username, password, userType, allUsers: users };
   });
 
-  server.post('/auth/login', async (request, reply) => {
-    const { username, password } = request.body as User;
-    const user = users.find(u => u.username === username && u.password === password);
-    if (user) {
-      return { status: 'success', username };
-    } else {
-      return { status: 'error', message: 'Invalid username or password' };
+  server.post('/auth/registerTailor', async (request, reply) => {
+    const {name, email, password, category = "universal"} = request.body as Tailor;
+    await pool.query('INSERT INTO tailors (name, email, password, category) VALUES ($1, $2, $3, $4)', [name, email, password, category]);
+
+    return {
+      status: "success",
+      user: {
+        name, email, password, category
+      }
     }
+  })
+
+  server.post('/auth/login', async (request, reply) => {
   });
 }
--- index.ts
+++ index.ts
@@ -1,15 +1,29 @@
 import Fastify from 'fastify'
 import authRoutes from "./auth"
+import { Pool } from "pg"
 
 const fastify = Fastify({
   logger: true
 })
 
+const pool = new Pool({
+  user: "lain",
+  host: "localhost",
+  database: "ecothon",
+  password: "iwakura",
+  port: 5432
+})
 
-authRoutes(fastify)
+
+authRoutes(fastify, pool)
 
 fastify.get('/', async (request, reply) => {
   return { hello: 'world' }
+})
+
+fastify.get("/users", async (request, reply) => {
+  const {rows} = await pool.query('SELECT * FROM users;')
+  return rows
 })
 
 const start = async () => {
