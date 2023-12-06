9ca4a918071a15e57180aac5357f3e1625993b79
cerealexperiments <prettysunshine468@gmail.com>
2023-12-07 03:34:49 +0600

    changes

--- index.ts
+++ index.ts
@@ -1,9 +1,16 @@
 import Fastify from 'fastify'
 import authRoutes from "./auth"
 import { Pool } from "pg"
+import multipart from "@fastify/multipart"
+import { pipeline } from 'stream';
+import fs from 'fs';
+import path from 'path';
+import staticPlugin from "@fastify/static"
+import { promisify } from 'util';
+
+const pump = promisify(pipeline)
 
 const fastify = Fastify({
-  logger: true
 })
 
 const pool = new Pool({
@@ -14,8 +21,49 @@ const pool = new Pool({
   port: 5432
 })
 
+fastify.register(multipart)
+fastify.register(staticPlugin, {
+  root: path.join(__dirname, 'uploads'),
+  prefix: '/uploads/', // this is the public URL prefix
+});
 
 authRoutes(fastify, pool)
+
+
+type PortfolioUploadBody = {
+  tailorId: number;
+}
+
+//file upload endpoints
+
+fastify.post("/uploadTailorPortfolio", async (request, reply) => {
+  const queryParameters = request.query as {
+    tailorId: string;
+  };
+
+  const tailorId = Number(queryParameters.tailorId)
+  console.log(tailorId)
+
+  const data = await request.file();
+  const newFilename = `tailor-${tailorId}-portfolio-${data!.filename}`
+  const filePath = path.join(__dirname, 'uploads', newFilename);
+
+  try {
+    await pump(data!.file, fs.createWriteStream(filePath));
+    // Here, you can save the file path (filename) to your database
+    // ...
+    await pool.query(
+      'UPDATE tailors SET portfolio = array_append(portfolio, $1) WHERE id = $2',
+      [newFilename, tailorId]
+    );
+
+    return { success: true, filePath };
+  } catch (err) {
+    console.error(err);
+    reply.code(500).send('Error processing file upload');
+  }
+}) 
+
 
 fastify.get('/', async (request, reply) => {
   return { hello: 'world' }
